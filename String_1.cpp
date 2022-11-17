e1633f7bd0d6b6bfdfc761066b2811843782f203
Goryunov Daniil <112342386+Danya05@users.noreply.github.com>
2022-11-17 13:49:45 +0300

    Update String_1.cpp

--- String_1.cpp
+++ String_1.cpp
@@ -6,41 +6,117 @@ class String {
   String() = default;
 
   String(const char* arg): string_(new char[strlen(arg) + 1]) {
-    size = strlen(arg);
-    string_ = new char[size + 1];
-    for (size_t i = 0; i < size; ++i) {
+    size_ = strlen(arg);
+    string_ = new char[size_ + 1];
+    for (size_t i = 0; i < size_; ++i) {
       string_[i] = *(arg + i);
     }
+    string_[size_] = '\0';
+    cap = 2 * size_;
   }
 
-  String(const String& str): size(str.size), cap(str.cap), string_(new char[str.cap + 1]) {
-    memcpy(string_, str.string_, size + 1);
+  String(const String& str): size_(str.size_), cap(str.cap), string_(new char[str.cap + 1]) {
+    memcpy(string_, str.string_, size_ + 1);
   }
 
-  String(size_t n, char c): size(n), cap(n), string_(new char[n + 1]) {
-    memset(string_, c, size);
+  String(size_t n, char c): size_(n), cap(n), string_(new char[n + 1]) {
+    memset(string_, c, size_);
     string_[n] = '\0';
+  }
+
+  size_t find(const String& st) {
+    char* result = new char[st.size_ + 1];
+    for (size_t i = 0; i < size_ - st.size_; ++i) {
+      memcpy(result, string_ + i, st.size_);
+      result[size_] = '\0';
+      if (String(result) == st) {
+        return i;
+      }
+    }
+    return size_;
+  }
+
+  size_t find(const String& st) const{
+    char* result = new char[st.size_ + 1];
+    for (size_t i = 0; i < size_ - st.size_; ++i) {
+      memcpy(result, string_ + i, st.size_);
+      result[size_] = '\0';
+      if (String(result) == st) {
+        return i;
+      }
+    }
+    return size_;
+  }
+
+  size_t rfind(const String& st) {
+    char* result = new char[st.size_ + 1];
+    for (size_t i = size_ - st.size_; i > 0; --i) {
+      memcpy(result, string_ + i, st.size_);
+      result[size_] = '\0';
+      if (String(result) == st) {
+        return i;
+      }
+    }
+    return size_;
+  }
+
+  size_t rfind(const String& st) const{
+    char* result = new char[st.size_ + 1];
+    for (size_t i = size_ - st.size_; i > 0; --i) {
+      memcpy(result, string_ + i, st.size_);
+      result[size_] = '\0';
+      if (String(result) == st) {
+        return i;
+      }
+    }
+    return size_;
+  }
+
+  char* data() {
+    return &string_[0];
+  }
+
+  char* data() const{
+    return &string_[0];
   }
 
   ~String() {
     delete[] string_;
   }
 
-  char* front() {
-    return &string_[0];
+  char& front() {
+    return string_[0];
   }
 
-  char* back() {
-    return &string_[size - 1];
+  const char& front() const {
+    return string_[0];
+  }
+
+  char& back() {
+    return string_[size_ - 1];
+  }
+
+  const char& back() const {
+    return string_[size_ - 1];
+  }
+
+  void push_back(char c) {
+    string_[size_] = c;
+    ++size_;
+  }
+
+  void pop_back() {
+    string_[size_] = '\0';
+    --size_;
   }
 
   bool empty() {
-    return size == 0;
+    return size_ == 0;
   }
 
   void clear() {
     if (!empty()) {
-      size = 0;
+      size_ = 0;
       delete[] string_;
       string_ = new char[1];
     }
@@ -48,50 +124,80 @@ class String {
 
   String substr(size_t start, size_t count) {
     String st;
-    st.size = count + 1;
-    st.string_ = new char[st.size];
+    st.size_ = count + 1;
+    st.string_ = new char[st.size_];
     for (size_t i = 0; i < count; ++i) {
       st.string_[i] = string_[start + i];
     }
     st.string_[count - 1] = '\0';
+    st.cap = st.size_;
     return st;
   }
 
-  static size_t length(const String& st) {
-    return st.size;
+  String substr(size_t start, size_t count) const {
+    String st;
+    st.size_ = count + 1;
+    st.string_ = new char[st.size_];
+    for (size_t i = 0; i < count; ++i) {
+      st.string_[i] = string_[start + i];
+    }
+    st.string_[count - 1] = '\0';
+    st.cap = st.size_;
+    return st;
   }
 
-  char operator[](int val) {
+  size_t length() {
+    return size_;
+  }
+
+  size_t length() const {
+    return size_;
+  }
+
+  char& operator[](int val) {
     return string_[val];
   }
 
-  /*void push_back(char c) {
-
+  const char& operator[](int val) const {
+    return string_[val];
   }
 
-  char* back(String string) {
-    return string[0];
-  }*/
+  size_t size() {
+    return size_;
+  }
+
+  size_t capacity() {
+    return cap;
+  }
+
+  void shrink_to_fit() {
+    clear();
+  }
+
+  String& operator+=(char st2);
+  String& operator+=(const String& st1);
 
  private:
-  size_t size = 0;
+  size_t size_ = 0;
   size_t cap = 0;
   char* string_ = nullptr;
-  //friend bool operator[](String& st);
-  friend String operator+(String& st1, String& st2);
-  friend String operator+=(String& st1, String& st2);
+  friend String operator+(const String& st1, const String& st2);
+  friend String operator+(char st1, const String& st2);
+  friend String operator+(const String& st1, char st2);
   friend bool operator==(const String& st1, const String& st2);
   friend bool operator>=(const String& st1, const String& st2);
   friend bool operator<=(const String& st1, const String& st2);
   friend bool operator>(const String& st1, const String& st2);
   friend bool operator<(const String& st1, const String& st2);
+  friend std::ostream& operator<<(std::ostream& os, const String& st);
+  friend std::istream& operator>>(std::istream& in, String& st);
 };
 
 bool operator==(const String& st1, const String& st2) {
-  if (st1.size != st2.size) {
+  if (st1.size_ != st2.size_) {
     return false;
   }
-  for (size_t i = 0; i < st1.size; ++i) {
+  for (size_t i = 0; i < st1.size_; ++i) {
     if (st1.string_[i] != st2.string_[i]) {
       return false;
     }
@@ -107,13 +213,13 @@ bool operator>=(const String& st1, const String& st2) {
 }
 
 bool operator>(const String& st1, const String& st2) {
-  if (st1.size > st2.size) {
+  if (st1.size_ > st2.size_) {
     return true;
   }
-  if (st1.size < st2.size) {
+  if (st1.size_ < st2.size_) {
     return false;
   }
-  for (size_t i = 0; i < st1.size; ++i) {
+  for (size_t i = 0; i < st1.size_; ++i) {
     if (st1.string_[i] > st2.string_[i]) {
       return true;
     }
@@ -126,29 +232,43 @@ bool operator>(const String& st1, const String& st2) {
 
 
 //НЕ О(1)
-String operator+=(String& st1, String& st2) {
-  st1.size = st1.size + st2.size;
-  st1.string_ = new char[st1.size + 1];
-  strcpy(st1.string_, st1.string_);
-  strcat(st1.string_, st2.string_);
-  return st1;
+String& String::operator+=(const String& st1) {
+  *this = *this + st1;
+  return *this;
 }
 
-String operator+(String& st1, String& st2) {
+String& String::operator+=(char st2) {
+  this -> push_back(st2);
+  return *this;
+}
+
+String operator+(const String& st1, const String& st2) {
   String st3;
-  st3.size = st1.size + st2.size;
-  st3.string_ = new char[st3.size + 1];
+  st3.size_ = st1.size_ + st2.size_;
+  st3.string_ = new char[st3.size_];
+  st3.cap = st1.cap + st2.cap;
   strcpy(st3.string_, st1.string_);
   strcat(st3.string_, st2.string_);
   return st3;
 }
 
-/*char& operator[](String& st, int i) {
-  if (length(st) > i) {
-    return 0;
-  }
-  return st.string_[i];
-}*/
+String operator+(char st1, const String& st2) {
+  String st3;
+  st3.size_ = 1 + st2.size_;
+  st3.string_ = new char[st3.size_ + 1];
+  st3[0] = st1;
+  strcpy(st3.string_, st2.string_);
+  return st3;
+}
+
+String operator+(const String& st1, char st2) {
+  String st3;
+  st3.size_ = 1 + st1.size_;
+  st3.string_ = new char[st3.size_ + 1];
+  st3[0] = st2;
+  strcpy(st3.string_, st1.string_);
+  return st3;
+}
 
 bool operator<=(const String& st1, const String& st2) {
   if (st1 == st2) {
@@ -158,13 +278,13 @@ bool operator<=(const String& st1, const String& st2) {
 }
 
 bool operator<(const String& st1, const String& st2) {
-  if (st1.size > st2.size) {
+  if (st1.size_ > st2.size_) {
     return false;
   }
-  if (st1.size < st2.size) {
+  if (st1.size_ < st2.size_) {
     return true;
   }
-  for (size_t i = 0; i < st1.size; ++i) {
+  for (size_t i = 0; i < st1.size_; ++i) {
     if (st1.string_[i] > st2.string_[i]) {
       return false;
     }
@@ -179,12 +299,15 @@ bool operator!=(const String& st1, const String& st2) {
   return !(st1 == st2);
 }
 
+std::ostream& operator<<(std::ostream& os, const String& st) {
+  return os << st.string_;
+}
 
-
-int main() {
-  char arg[6] = { "abcde" };
-  String st(arg);
-  String a(5, 'a');
-  st.clear();
-  std::cout << st.empty();
+std::istream& operator>>(std::istream& in, String& st) {
+  char array[1000];
+  in >> array;
+  st.size_ = strlen(array);
+  st.string_ = new char[st.size_ + 1];
+  strcpy(st.string_, array);
+  return in;
 }
